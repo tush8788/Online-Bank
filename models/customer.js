278be59856599fb8df924e4632f913132fe92441
tush87 <tusharj8788@gmail.com>
2023-02-26 18:35:31 +0530

    add schema

---
+++ models/customer.js
@@ -0,0 +1,45 @@
+const mongoose=require('mongoose');
+
+const customerSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    adharNo:{
+        type:Number,
+        unique:true
+    },
+    panNo:{
+        type:Number,
+        unique:true
+    },
+    address:{
+        type:String
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isSaving:{
+        type:Boolean
+    },
+    balance:{
+        type:Number,
+        default: 0
+    },
+    creditScore:{
+        type:Number
+    }
+},{timestamps:true});
+
+const Customer=mongoose.model("Customer",customerSchema);
+
+module.exports=Customer;
