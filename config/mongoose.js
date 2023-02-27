3cc78a8b147eb15d92f4c1e662f782483091a2c3
Tushar Rajendra Jagtap <56924495+tush8788@users.noreply.github.com>
2023-02-27 10:01:09 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const dotenv=require('dotenv').config();
 mongoose.set('strictQuery',false);
 
-mongoose.connect(process.env.MONGO_URL||'mongodb://localhost/ippopay-assignment');
+mongoose.connect(process.env.MONGO_URL);
 
 const db=mongoose.connection;
 
